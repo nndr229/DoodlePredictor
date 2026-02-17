1dd38ae04a0f2d9abcb5285c788058d777109b86
Nithin Deepkumar <nithindeepkumar@Nithins-MacBook-Pro.local>
2026-02-17 20:39:41 +0530

    chore: optimize code (Score: 64/100)

--- covert_image_to_array.py
+++ covert_image_to_array.py
@@ -4,41 +4,41 @@ import numpy as np
 from skimage.transform import resize
 
 im = Image.open("my_drawing.bmp")
-newsize = (28, 28) 
-im = im.resize(newsize, Image.ANTIALIAS) 
+newsize = (28, 28)
+im = im.resize(newsize, Image.ANTIALIAS)
 
 im.show()
 
-im2= im.convert("1")
+im2 = im.convert("1")
 ary = np.array(im2)
 im2.show()
 
-output_array=[]
-zero=ary[0][0]
+output_array = []
+zero = ary[0][0]
 
 for j in range(len(ary)):
-	for i in range(28):
-		output_array.append(ary[j][i])	
+    for i in range(28):
+        output_array.append(ary[j][i])
 
 for i in range(len(output_array)):
-	if output_array[i]==zero:
-		output_array[i]=0
-	else:
-		output_array[i]=1	
+    if output_array[i] == zero:
+        output_array[i] = 0
+    else:
+        output_array[i] = 1
 
-img = Image.new('1', (28, 28))
+img = Image.new("1", (28, 28))
 pixels = img.load()
 
-data=output_array
+data = output_array
 print("------------------------------")
 print("------------------------------")
 print("------------------------------")
 
-data[:] = [data[i:i + 28] for i in range(0, 784,28)]
+data[:] = [data[i : i + 28] for i in range(0, 784, 28)]
 
 for i in range(img.size[0]):
     for j in range(img.size[1]):
-        pixels[i, j] = data[i][j],
-        
-img = img.rotate(270, PIL.Image.NEAREST, expand = 1) 
+        pixels[i, j] = (data[i][j],)
+
+img = img.rotate(270, PIL.Image.NEAREST, expand=1)
 img.show()
--- matrix.py
+++ matrix.py
@@ -4,123 +4,117 @@ import numpy as np
 
 
 class Matrix:
-	def __init__(self, rows, cols):
-		self.rows = rows
-		self.cols = cols
-		self.data = []
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = []
 
-		for i in range(self.rows):
-			self.data.append([])
-			inner_row = self.data[i]
-			for j in range(self.cols):
-				inner_row.append(0)
+        for i in range(self.rows):
+            self.data.append([])
+            inner_row = self.data[i]
+            for j in range(self.cols):
+                inner_row.append(0)
 
-	
+    def get_matrix(self):
+        if self.rows > 0 and self.cols > 0:
+            for i in range(len(self.data)):
+                print(self.data[i])
+            print()
+            return self.data
 
-	def get_matrix(self):
-		if self.rows > 0 and self.cols > 0:
-			for i in range(len(self.data)):
-				print(self.data[i])
-			print()
-			return self.data
+    def scalar_multiply(self, n):
+        if isinstance(n, Matrix):
+            for i in range(len(self.data)):
+                for j in range(len(self.data[i])):
+                    self.data[i][j] *= n.data[i][j]
+        else:
+            for i in range(len(self.data)):
+                for j in range(len(self.data[i])):
+                    self.data[i][j] *= n
+        return self.data
 
-	def scalar_multiply(self, n):
-		if isinstance(n, Matrix):
-			for i in range(len(self.data)):
-				for j in range(len(self.data[i])):
-					self.data[i][j] *= n.data[i][j]
-		else:
-			for i in range(len(self.data)):
-				for j in range(len(self.data[i])):
-					self.data[i][j] *= n
-		return self.data
+    def mapper(self, func):
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                val = self.data[i][j]
+                self.data[i][j] = func(val)
+        return self.data
 
-	def mapper(self,func):
-		for i in range(len(self.data)):
-			for j in range(len(self.data[i])):
-				val = self.data[i][j]
-				self.data[i][j] = func(val)
-		return self.data
+    @staticmethod
+    def map(matrix, func):
+        result = Matrix(matrix.rows, matrix.cols)
+        for i in range(len(matrix.data)):
+            for j in range(len(matrix.data[i])):
+                val = matrix.data[i][j]
+                result.data[i][j] = func(val)
 
+        return result
 
-	@staticmethod
-	def map(matrix,func):
-		result = Matrix(matrix.rows, matrix.cols)
-		for i in range(len(matrix.data)):
-			for j in range(len(matrix.data[i])):
-				val = matrix.data[i][j]
-				result.data[i][j] = func(val)
+    @staticmethod
+    def fromArray(arr):
+        m = Matrix(len(arr), 1)
+        for i in range(len(arr)):
+            m.data[i][0] = arr[i]
 
-		return result
-						 
-	@staticmethod
-	def fromArray(arr):
-		m = Matrix(len(arr),1)
-		for i in range(len(arr)):
-			m.data[i][0]=arr[i]
+        return m
 
-		return m	
-	@staticmethod
-	def subtract(a,b):
-		result = Matrix(a.rows,a.cols)
-		for i in range(len(result.data)):
-			for j in range(len(result.data[i])):
-				result.data[i][j] = a.data[i][j] - b.data[i][j] 
-		return result
+    @staticmethod
+    def subtract(a, b):
+        result = Matrix(a.rows, a.cols)
+        for i in range(len(result.data)):
+            for j in range(len(result.data[i])):
+                result.data[i][j] = a.data[i][j] - b.data[i][j]
+        return result
 
+    def add(self, n):
+        if isinstance(n, Matrix):
+            for i in range(len(self.data)):
+                for j in range(len(self.data[i])):
+                    self.data[i][j] += n.data[i][j]
+        else:
+            for i in range(len(self.data)):
+                for j in range(len(self.data[i])):
+                    self.data[i][j] += n
+        return self.data
 
+    def toArray(self):
+        arr = []
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                arr.append(self.data[i][j])
+        return arr
 
-	def add(self, n):
-		if isinstance(n, Matrix):
-			for i in range(len(self.data)):
-				for j in range(len(self.data[i])):
-					self.data[i][j] += n.data[i][j]
-		else:
-			for i in range(len(self.data)):
-				for j in range(len(self.data[i])):
-					self.data[i][j] += n
-		return self.data
+    def randomize(self):
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                self.data[i][j] = random.uniform(-1, 1)
+                # self.data[i][j] = math.floor(self.data[i][j])
 
-	def toArray(self):
-		arr =[]
-		for i in range(len(self.data)):
-			for j in range(len(self.data[i])):
-				arr.append(self.data[i][j])
-		return arr		
-		
+    @staticmethod
+    def multiply(m, n):
+        if type(n) == Matrix:
+            if m.cols != n.rows:
+                print("Columns of A must match the rows of B")
+            else:
+                result = Matrix(m.rows, n.cols)
+                for i in range(result.rows):
+                    for j in range(result.cols):
+                        sum = 0
+                        for k in range(m.cols):
+                            sum += m.data[i][k] * n.data[k][j]
+                        result.data[i][j] = sum
+            return result
 
-	def randomize(self):
-		for i in range(len(self.data)):
-			for j in range(len(self.data[i])):
-				self.data[i][j] = random.uniform(-1,1)
-				# self.data[i][j] = math.floor(self.data[i][j])
+    @staticmethod
+    def transpose(a):
+        new = Matrix(a.cols, a.rows)
 
-	@staticmethod
-	def multiply(m,n):
-		if type(n) == Matrix:
-			if(m.cols != n.rows):
-				print("Columns of A must match the rows of B")
-			else:
-				result = Matrix(m.rows,n.cols)
-				for i in range(result.rows):
-					for j in range(result.cols):
-						sum = 0
-						for k in range(m.cols):
-							sum += m.data[i][k] * n.data[k][j]
-						result.data[i][j] =sum
-			return result			
-	@staticmethod
-	def transpose(a):
-		new = Matrix(a.cols, a.rows)
-
-		for i in range(len(new.data)):
-			for j in range(len(new.data[i])):
-				new.data[i][j] = a.data[j][i]
-		return new
+        for i in range(len(new.data)):
+            for j in range(len(new.data[i])):
+                new.data[i][j] = a.data[j][i]
+        return new
 
 
 def double(*args):
-	for arg in args:
-		return 2*arg
-
-
+    for arg in args:
+        return 2 * arg
